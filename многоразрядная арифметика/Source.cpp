1acd2230761edd32677c07c67c68caddf0a54acd
jabkevich <Dmitry.jabkevich@gmail.com>
2019-01-06 21:43:26 +1000

    Проект сдан

--- многоразрядная арифметика/Source.cpp
+++ многоразрядная арифметика/Source.cpp
@@ -20,48 +20,45 @@ struct separation_String //структуры для хранения числа в строке
 	int Nn_to;//количество цифр в последнем разряде до запятой
 	int Nn_after;//количество цифр в последнем разряде после запятой
 };
-bool ChekLong(string num);
+int ChekLong(string num);
 void EQLONG(separation_String &A, separation_String& B);
-bool ReadLong(separation_String &numA, separation_String &
-	numB) {
+int numb_str=0;
+bool ReadLong(ifstream &infile, separation_String &num, string &Eror) {
 	int amount_of_numbers = 0;
-	string buffer;
-	bool chek;//переменная для проверк числа в файле
-	ifstream infile("input.TXT");
-	while (!infile.eof())
+	if (infile)
 	{
+		
+	
+	string buffer;
+	
+	int chek;//переменная для проверк числа в файле
+	numb_str++;
 		infile >> buffer;
-		chek = ChekLong(buffer);
-		if ((chek == true) && (amount_of_numbers == 0)) {
-			numA.NUM = buffer;
-			numA.NUM1 = buffer;
-			numA.to_count = count_to;
-			numA.after_count = count_after;
-			amount_of_numbers++;
-		}
-		else if ((chek == true) && (amount_of_numbers == 1)) {
-			numB.NUM = buffer;
-			numB.NUM1 = buffer;
-			numB.to_count = count_to;
-			numB.after_count = count_after;
-			amount_of_numbers++;
-		}
-		if (amount_of_numbers == 2) {
-			EQLONG(numA, numB);
-			break;
-		}
-	}
-	if (amount_of_numbers != 2) {
-		switch (amount_of_numbers)
+		if (buffer == "")
 		{
-		case 1: cout << "найденно только одно число. Выход из программы"; system("pause"); return false;
-		case 0: cout << "не найденно ни одного числа. Выход из программы"; system("pause"); return false;
+			Eror = "файл пуст \n"; return false;
 		}
-	}
+		chek = ChekLong(buffer);
+		if (chek == 0) {
+			num.NUM = buffer;
+			num.NUM1 = buffer;
+			num.to_count = count_to;
+			num.after_count = count_after;
+		}
+		else {
+			switch (chek)
+			{
+			case 1: Eror = "в строке  "; Eror += std::to_string(numb_str); Eror += " неккоректное вещественное число \n"; return false;
+			case 2: Eror = "в строке  "; Eror += std::to_string(numb_str); Eror += " больше 99 символов \n"; return false;
+			}
+		}
+		return true;
+		
+	}else if (!infile){  Eror = "файл не открывается\n"; return false; }
 }
-bool ChekLong(string num)//проверка числа. Я НЕ РАЗОБРАЛСЯ В МНОЖЕСТВАХ, ТАМ КАКАЯ ТО ЕБАЛА, ПО ЭТОМУ ТАК
+int ChekLong(string num)//проверка числа. Я НЕ РАЗОБРАЛСЯ В МНОЖЕСТВАХ, ТАМ КАКАЯ ТО ЕБАЛА, ПО ЭТОМУ ТАК
 {
-	bool chek = true;
+	int chek =0;
 	int point = 0;
 	int  count; //count_to-счетчик значений до знака. count_after-счтетчик значений после знка 
 	int g = num.size();
@@ -86,17 +83,17 @@ bool ChekLong(string num)//проверка числа. Я НЕ РАЗОБРАЛСЯ В МНОЖЕСТВАХ, ТАМ КАК
 			count_after++;
 		else
 			count_to++;
-		if (count == 0) return false;
+		if (count == 0) return 1;
 	}
 	if (num[0] == '0') {
 		for (int i = 0; i < num.find('.'); i++) {
-			if (num[i] != '0')return false;
+			if (num[i] != '0')return 1;
 		}
 	}
 	g = num.size();
-	if ((num[0]=='.')||(num[g-1]=='.')) return false;
-	if ((point == 0) || (point > 1)) chek = false;// если запятая не одна, то выход
-	if (num.size() > 99) return false;
+	if ((num[0]=='.')||(num[g-1]=='.')) return 1;
+	if ((point == 0) || (point > 1)) return 1;// если запятая не одна, то выход
+	if ((num.find('.')>99) || (num.size() - num.find('.')) > 99)return 2;
 	return chek;
 }
 void PrintLong(separation_String &Astr)//переворачивает чсило, разбиваю на разряды по 4
@@ -151,6 +148,9 @@ void chek(int &i, int &j, string NUM[MAX], int &n)
 }
 void ADD(separation_String &A, separation_String &B, separation_String &C)//Сложение чисел
 {
+	EQLONG(A, B);
+	PrintLong(A);// переворачиваем число A
+	PrintLong(B);//переворачиваем число B
 	char a1;
 	int g = A.after_comma[0].size();
 	int a = 0, b = 0, c = 0; // переменные в которые я буду класть число из строки
@@ -195,22 +195,31 @@ void ADD(separation_String &A, separation_String &B, separation_String &C)//Слож
 		{ 
 			if (A.to_comma[i].size()==4)
 			C.to_comma[i+1] +=std::to_string(c);
-			else C.to_comma[i ] += std::to_string(c);
-			C.N_to = i + 1;
+			else C.to_comma[i] += std::to_string(c);
+			C.N_to += 1;
 		}	
 	}
+
 	for (int i = 0; i < C.N_after; i++)
 	{
-		C.NUM += C.after_comma[i];
+		C.NUM += C.after_comma[i];	
 	}
 	C.NUM += ".";
+	g = 0;
 	for (int i = 0; i < C.N_to; i++)
 	{
+		
 		C.NUM += C.to_comma[i];
 	}
 	for (int j = 0; j < C.NUM.size() / 2; j++) { //переворачиваю число 
 
 		swap(C.NUM[j], C.NUM[C.NUM.size() - j - 1]);
+	}
+	for (int j = C.NUM.size()-1; j >0 ; j--) {
+		if ((C.NUM[j] == '0')&&(C.NUM[j-1] != '.')) {
+			C.NUM.erase(j - 1, 1);  
+		}
+		else break;
 	}
 }
 bool LessLong(separation_String &A, separation_String &B)//процедура проверки большего числа
@@ -343,6 +352,11 @@ void SubLong(separation_String &A, separation_String &B, separation_String &C)
 		else break;
 	}
 }
+void print_F( separation_String &num, ofstream &outfile) {
+
+	outfile  <<num.NUM << "\n";
+	
+}
 int main()
 {
 	setlocale(LC_ALL, "rus");
@@ -350,21 +364,21 @@ int main()
 	separation_String numB;//Число B
 	separation_String SummC;//сумма чисел A и B
 	separation_String diffC;//разность чисел A и B
-	ofstream outfile("output.TXT");//Выходной файл
-	if (ReadLong(numA, numB) == false) return 0;
-	PrintLong(numA);// переворачиваем число A
-	PrintLong(numB);//переворачиваем число B
+	ifstream infile("input.TXT");
+	ofstream outfile("output.TXT");
+	string eror;
+	if (ReadLong(infile, numA, eror) == false) {cout << eror; system("pause"); return 0;}
+	if (ReadLong(infile, numB, eror) == false) {cout << eror; system("pause"); return 0;}
 	ADD( numA, numB, SummC);//складываем числа
-	outfile << numA.NUM1 <<" + "<<numB.NUM1<<" = " << SummC.NUM << "\n";
-    if (LessLong(numA, numB)==true) {
+	if (LessLong(numA, numB) == true) {
 		SubLong(numA, numB, diffC);
-		outfile << numA.NUM1 << " - " << numB.NUM1 << " = " << diffC.NUM;
 	}
-	else { 
+	else {
 		SubLong(numB, numA, diffC);
-	    outfile << numB.NUM1 << " - " << numA.NUM1 << " = " << diffC.NUM; 
 	}
-
+	print_F(SummC, outfile);
+	print_F(diffC, outfile);
+	
 	system("pause");
 	return 0;
 }
